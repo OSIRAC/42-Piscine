b2a3097dbaa5056d0481a2ac0e7feeba6ec3ee39
Ömer Sirac Islamoğlu <oislamog@k1m28s08.42istanbul.com.tr>
2024-11-10 20:49:45 +0300

    C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oislamog <oislamog@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/28 20:14:12 by oislamog          #+#    #+#             */
+/*   Updated: 2024/01/30 12:57:16 by oislamog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
+			ft_putchar(32);
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
+			if (a != 98)
+			{
+				write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
+}
